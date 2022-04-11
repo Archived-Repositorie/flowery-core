45075591a55673214e9e71348ce7aa7fb4c54765
JustFoxx <justfoxxo@outlook.com>
2022-04-11 10:42:05 +0200

    Don't register

--- forge/src/main/kotlin/io/github/justfoxx/StructureFeature.kt
+++ forge/src/main/kotlin/io/github/justfoxx/StructureFeature.kt
@@ -1,15 +1,23 @@
 package io.github.justfoxx
 
+import io.github.justfoxx.mixin.StructureFeatureAccessor
 import io.github.justfoxx.structure.WaterStructures
+import net.minecraft.world.gen.GenerationStep
 import net.minecraft.world.gen.feature.StructureFeature
 import net.minecraftforge.registries.ForgeRegistries
-import java.util.function.Supplier
 
 class StructureFeature {
+    val WATER_STRUCTURE = WaterStructures()
     val DeferredRegistering = net.minecraftforge.registries.DeferredRegister.create<StructureFeature<*>>(ForgeRegistries.STRUCTURE_FEATURES, MODID)
     fun Register() {
-        DeferredRegistering.register("water", Supplier {
-            WaterStructures()
-        })
+
+        DeferredRegistering.register("water") {
+            StructureFeatureAccessor.callRegister(
+                "${MODID}:water",
+                WATER_STRUCTURE,
+                GenerationStep.Feature.SURFACE_STRUCTURES
+            )
+        }
+
     }
 }
