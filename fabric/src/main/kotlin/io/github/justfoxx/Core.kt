c49c1b78498098357cb30b30751a8a242d09bd99
JustFoxx <justfoxxo@outlook.com>
2022-06-15 19:27:34 +0200

    fix

--- fabric/src/main/kotlin/io/github/justfoxx/Core.kt
+++ fabric/src/main/kotlin/io/github/justfoxx/Core.kt
@@ -1,15 +1,19 @@
 package io.github.justfoxx
 
+import net.fabricmc.api.ModInitializer
 import net.fabricmc.loader.api.FabricLoader
 
-@Suppress("unused")
-fun init() {
-    Structure().registerStructureFeatures()
-    for(mod in supported) {
-        if (FabricLoader.getInstance().isModLoaded(mod.key) && !FabricLoader.getInstance().isModLoaded(mod.value)) {
-            Logger.warn("Mod ${mod.key} is loaded but ${mod.key} support mod is not loaded")
-        } else if(FabricLoader.getInstance().isModLoaded(mod.value)) {
-            Logger.info("Support mod ${mod.value} has been loaded")
+@Suppress("UNUSED")
+object Core : ModInitializer {
+    override fun onInitialize() {
+        Structure()
+        for(mod in supported) {
+            if (FabricLoader.getInstance().isModLoaded(mod.key) && !FabricLoader.getInstance().isModLoaded(mod.value)) {
+                Logger.warn("Mod ${mod.key} is loaded but ${mod.key} support mod is not loaded")
+            } else if(FabricLoader.getInstance().isModLoaded(mod.value)) {
+                Logger.info("Support mod ${mod.value} has been loaded")
+            }
         }
     }
 }
+
